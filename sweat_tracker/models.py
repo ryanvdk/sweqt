81b353864bb56969284a55f8f7a36824e3086802
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-31 16:37:33 -0700

    Started to set up models.

--- sweat_tracker/models.py
+++ sweat_tracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Organizations(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Contributor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Teams(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Contributions(models.Model):
+    contribution_type = models.CharField(max_length=50)
+    description = models.TextField()
+    work_hours = models.FloatField()
+    work_units = models.FloatField()
